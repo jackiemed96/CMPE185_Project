816aa5855cb3e2776dd54945c7a2010e20e52959
jackie <jackie.med96@yahoo.com>
2022-12-13 22:27:24 -0800

    adding rosbag launch file and updated changes

---
+++ build/camera_node/CMakeFiles/camera_node_exe.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jackie/Desktop/CMPE185/CMPE185_Project/CMPE185_ws/src/camera_node/src/camera.cpp" "/home/jackie/Desktop/CMPE185/CMPE185_Project/build/camera_node/CMakeFiles/camera_node_exe.dir/src/camera.cpp.o"
+  "/home/jackie/Desktop/CMPE185/CMPE185_Project/CMPE185_ws/src/camera_node/src/camera_node.cpp" "/home/jackie/Desktop/CMPE185/CMPE185_Project/build/camera_node/CMakeFiles/camera_node_exe.dir/src/camera_node.cpp.o"
+  "/home/jackie/Desktop/CMPE185/CMPE185_Project/CMPE185_ws/src/camera_node/src/camera_node_main.cpp" "/home/jackie/Desktop/CMPE185/CMPE185_Project/build/camera_node/CMakeFiles/camera_node_exe.dir/src/camera_node_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEFAULT_RMW_IMPLEMENTATION=rmw_cyclonedds_cpp"
+  "RCUTILS_ENABLE_FAULT_INJECTION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jackie/Desktop/CMPE185/CMPE185_Project/CMPE185_ws/src/camera_node/include"
+  "/usr/include/opencv4"
+  "/opt/ros/galactic/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/camera_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jackie/Desktop/CMPE185/CMPE185_Project/CMPE185_ws/src/camera_node
+# Build directory: /home/jackie/Desktop/CMPE185/CMPE185_Project/build/camera_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
